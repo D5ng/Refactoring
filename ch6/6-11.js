ae67b9befb012265e7dec4aaaef1ab3127287066
d5ng <dong5ffice@gmail.com>
2023-03-22 17:08:22 +0900

    단계 쪼개기 1

---
+++ ch6/6-11.js
@@ -0,0 +1,36 @@
+export function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const calculateDiscount = discount(product, quantity);
+  const shippingCost = calculateShippingCost(basePrice, shippingMethod, quantity);
+  
+  return basePrice - calculateDiscount + shippingCost;
+}
+
+function discount(product, quantity){
+  return Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate
+}
+
+function calculateShippingCost(basePrice, shippingMethod, quantity) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  return shippingPerCase * quantity;
+}
+
+
+// 사용 예:
+const product = {
+  basePrice: 10,
+  discountRate: 0.1,
+  discountThreshold: 10,
+};
+
+const shippingMethod = {
+  discountThreshold: 20,
+  feePerCase: 5,
+  discountedFee: 3,
+};
+
+const price = priceOrder(product, 5, shippingMethod);
+console.log(price);
