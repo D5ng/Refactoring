009898195464cc669c29adac7cf71d3deb512e1a
d5ng <dong5ffice@gmail.com>
2023-03-26 16:47:22 +0900

    필드 옮기기

---
+++ ch8/8-2-2.js
@@ -0,0 +1,25 @@
+class Account {
+  #number;
+  #type;
+  constructor(number, type) {
+    this.#number = number;
+    this.#type = type;
+  }
+
+  get interestRate() {
+    return this.#type.interestRate;
+  }
+}
+
+class AccountType {
+  #name;
+  #interestRate;
+  constructor(nameString, interestRate) {
+    this.#name = nameString;
+    this.#interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this.#interestRate;
+  }
+}
