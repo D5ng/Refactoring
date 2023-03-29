9e6bcc20ecc565bbc8a0ddcacae2af2cdbfd6a90
d5ng <dong5ffice@gmail.com>
2023-03-29 14:17:01 +0900

    조건식 통합하기

---
+++ ch10/10-2.js
@@ -0,0 +1,14 @@
+function disabilityAmount(employee) {
+  if (isNotEligibleForDesability(employee)) {
+    return 0;
+  }
+  return 1;
+}
+
+function isNotEligibleForDesability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
+}
